55f849802df98810d7be8677fac6d6b4aab24ce1
BellaTamang <belamoktan07@gmail.com>
2017-06-12 10:27:40 +0530

    Create ShoppingCart.java

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -7,18 +7,21 @@ import java.util.List;
 
 public class ShoppingCart {
 
-	private List<Product> productList = new ArrayList<>();
+	private List<Product> productList = new ArrayList<>(); // created a arraylist of name productList 
 	private double totalCartValue;
 
-	public int getProductCount() {
+	public int getProductCount() {        // will count the number of product entered and return it as the size of arralist
 		return productList.size();
 	}
 
-	public void addProduct(Product product) {
+	public void addProduct(Product product) { // will add the product t the arraylist
 		productList.add(product);
 	}
-
-	public double getTotalCartValue() {
+	
+      /*will give the total sum of the product added in the arraylist
+      have used try and catch block to hanlde the excptions */
+	
+	public double getTotalCartValue() {  
 		try {
 		if (productList.size() > 0) {
 			for (Product product : productList) {
